a6a4dc2a380894107327a2cae24d0684b5a473f8
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-03-06 17:33:48 +0530

    1 change remain

---
+++ script.js
@@ -0,0 +1,42 @@
+let display = document.getElementById("inp");
+let string = "";
+
+function isOperator(char) {
+  const operators = ["+", "-", "*", "/"];
+  return operators.includes(char);
+}
+
+function myfun(input) {
+    let currentValue = display.value;
+  
+    // If the input is 'x', treat it as '*'
+    if (input === 'x') {
+      input = '*';
+    }
+  
+    // Check if the input is an operator and the last character is also an operator
+    if (isOperator(input) && isOperator(currentValue[currentValue.length - 1])) {
+      // If true, replace the last operator with the new one
+      string = currentValue.slice(0, -1);
+    } else {
+      string = currentValue;
+    }
+  
+    // Append the input to the display
+    string += input;
+    display.value = string;
+  }
+
+function mydelete() {
+  string = string.substring(0, string.length - 1);
+  display.value = string;
+}
+
+function myclear() {
+  display.value = "";
+}
+
+function calculate() {
+  string = eval(string);
+  display.value = string;
+}
